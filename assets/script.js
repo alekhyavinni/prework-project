794d249e8f2c236c4cf8500f94ceb92274c57909
bootcamp vinni <alekhyavinni12@gmail.com>
2023-03-21 20:28:46 -0400

    added js file

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,36 @@
-var topic = "Javascript";
+/*var topics = "Javascript";*/
 var topics = ["HTML","Javascript","css","git"];
+var randomTopic = topics[Math.floor(Math.random()*topics.length)];
 
-console.log(topic);
+console.log("here are the topics we learned through prework");
 
-if (topic == 'HTML')
+function selecttopic(){
+if (randomTopic == 'HTML')
 {
     console.log("lets study html");
 }
-else if(topic == 'css'){
-    console.log("lest code css");
+else if(randomTopic == 'css'){
+    console.log("lets code css");
 }
-else if(topic == 'git'){
+else if(randomTopic == 'git'){
     console.log("lets study git");
 }
-else if(topic == 'Javascript'){
+else if(randomTopic == 'Javascript'){
     console.log("lets code javascript")
 }
 else{
     console.log("please try again");
 }
+}
+
+function listtopics(){
 
 for(var x = 0;x < topics.length ; x++){
     console.log(topics[x]);
 }
+}
+
+listtopics();
+console.log("which topic should we learn first?");
+selecttopic();
+
